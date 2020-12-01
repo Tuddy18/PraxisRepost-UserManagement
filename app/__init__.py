69f206ac9f88ad8cb020104de8afc1ab8e617013
tuddy <tuddy18_26@yahoo.com>
2020-12-01 19:46:24 +0200

    updated cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from db_config import config
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 db_url = 'mysql://' + config['MYSQL_USER'] + ':' + config['MYSQL_PASSWORD'] + '@' + config['MYSQL_HOST'] + '/' + config['MYSQL_DB']
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
